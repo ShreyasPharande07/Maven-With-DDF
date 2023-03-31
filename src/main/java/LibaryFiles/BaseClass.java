543f38847f128d0f93a5135207549331a187cb31
shrey <shrey@SHREYA>
2023-03-31 15:26:33 +0530

    s

--- src/main/java/LibaryFiles/BaseClass.java
+++ src/main/java/LibaryFiles/BaseClass.java
@@ -5,14 +5,19 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass 
 {
 	public WebDriver driver;
+	public ChromeOptions co;
+	
 @SuppressWarnings("deprecation")
 public void InitiallizeBrowser() throws IOException 
 {
-	driver=new ChromeDriver();
+	co=new ChromeOptions();
+	co.addArguments("--remote-allow-origins=*");
+	driver=new ChromeDriver(co);
 	driver.get(UtilityClass.PropertyFile("URL"));
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
